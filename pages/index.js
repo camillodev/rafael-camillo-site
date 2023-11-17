20f4c94be5b612bcf9569115d198fa54927cba91
Rafael Camillo <camillodeveloper@gmail.com>
2023-11-17 17:41:25 -0300

    Add code and business post categories

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,8 @@ export async function getStaticProps({ locale, defaultLocale, locales }) {
 
 export default function Home({ posts, locale, availableLocales }) {
   const { t } = useTranslation()
+  const codePosts = posts.filter((p) => p.tags.find((t) => t === 'code'))
+  const businessPosts = posts.filter((p) => p.tags.find((t) => t === 'business'))
 
   return (
     <>
@@ -75,6 +77,75 @@ export default function Home({ posts, locale, availableLocales }) {
         </ul>
       </div>
 
+      <div className="post-category">
+        <div className="section-header flex justify-between pt-12">
+          <h1 className="flex text-3xl  font-extrabold leading-6">{t('common:code')}</h1>
+          {codePosts.length > MAX_DISPLAY && (
+            <div className=" justify-end text-base font-medium leading-6">
+              <Link
+                href="/blog"
+                className="more text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                aria-label="all posts"
+              >
+                {t('common:all')}
+              </Link>
+            </div>
+          )}
+        </div>
+        <ul className="grid grid-cols-3 gap-4 ">
+          {!codePosts.length && 'No posts found.'}
+          {codePosts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+            const { slug, date, title, summary, tags, image } = frontMatter
+            return (
+              <li key={slug} className="py-12">
+                <Post
+                  slug={slug}
+                  date={date}
+                  title={title}
+                  summary={summary}
+                  tags={tags}
+                  image={image}
+                />{' '}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+
+      <div className="post-category">
+        <div className="section-header flex justify-between pt-12">
+          <h1 className="flex text-3xl  font-extrabold leading-6">{t('common:business')}</h1>
+          {codePosts.length > MAX_DISPLAY && (
+            <div className=" justify-end text-base font-medium leading-6">
+              <Link
+                href="/blog"
+                className="more text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+                aria-label="all posts"
+              >
+                {t('common:all')}
+              </Link>
+            </div>
+          )}
+        </div>
+        <ul className="grid grid-cols-3 gap-4 ">
+          {!businessPosts.length && 'No posts found.'}
+          {businessPosts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+            const { slug, date, title, summary, tags, image } = frontMatter
+            return (
+              <li key={slug} className="py-12">
+                <Post
+                  slug={slug}
+                  date={date}
+                  title={title}
+                  summary={summary}
+                  tags={tags}
+                  image={image}
+                />{' '}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
       {/* {siteMetadata.newsletter.provider !== '' && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm title={t('newsletter:title')} />
